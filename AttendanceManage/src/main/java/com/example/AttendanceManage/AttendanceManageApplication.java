e97d21c81b6d00d84eea61626e1faea0e5a3f67e
kokiishikawa <koki.ishikawa0321@gmail.com>
2023-11-15 10:35:39 +0900

    HTML雛形修正

---
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/AttendanceManageApplication.java
@@ -0,0 +1,18 @@
+package com.example.AttendanceManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// http://localhost:8080/
+// http://localhost:8080/attendanceList
+
+@SpringBootApplication
+public class AttendanceManageApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AttendanceManageApplication.class, args);
+	}
+
+}
