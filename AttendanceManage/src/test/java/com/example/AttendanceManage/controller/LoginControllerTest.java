9b5f3ddc54550766b6c9956244c95e1f55c3927d
kokiishikawa <koki.ishikawa0321@gmail.com>
2023-11-28 23:16:17 +0900

    Testファイ作成

---
+++ AttendanceManage/src/test/java/com/example/AttendanceManage/controller/LoginControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.AttendanceManage.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @Test
+    void loginPage() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void indexPage() {
+    }
+}
