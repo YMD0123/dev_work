1cde0f46fb510c5fb28a235bfe3e2af3c3d12496
Gutsu4 <your_email@example.com>
2023-12-04 12:24:14 +0900

    セッションひな型作成

--- AttendanceManage/src/main/java/com/example/AttendanceManage/model/Attendance.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/model/Attendance.java
@@ -15,27 +15,4 @@ public class Attendance {
     private LocalTime startTime;
     private LocalTime endTime;
     private int breakDuration;
-
-
-
-    public void setId(Integer id) {
-    }
-
-    public void setUserId(Integer userId) {
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-    }
-
-    public void setDate(LocalDate date) {
-    }
-
-    public void setStartTime(LocalTime startTime) {
-    }
-
-    public void setEndTime(LocalTime endTime) {
-    }
-
-    public void setBreakDuration(Integer breakDuration) {
-    }
 }
