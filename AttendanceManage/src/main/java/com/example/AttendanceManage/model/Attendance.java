150dbc9c8bc5574dc27b1255ffec4cf8088efc93
kokiishikawa <koki.ishikawa0321@gmail.com>
2023-12-06 09:56:06 +0900

    打刻機能

--- AttendanceManage/src/main/java/com/example/AttendanceManage/model/Attendance.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/model/Attendance.java
@@ -1,90 +1,21 @@
 package com.example.AttendanceManage.model;
 
-import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
+//import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-//@Getter
-//@Setter
-@Data
+@Getter
+@Setter
 public class Attendance {
 
     private int id;
     private int userId;
     private String departmentCode;
     private LocalDate date;
+    private LocalTime startTime;
     private LocalTime endTime;
     private int breakDuration;
 
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    private int id;
-//
-//    public int getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(int userId) {
-//        this.userId = userId;
-//    }
-//
-//    private int userId;
-//
-//    public String getDepartmentCode() {
-//        return departmentCode;
-//    }
-//
-//    public void setDepartmentCode(String departmentCode) {
-//        this.departmentCode = departmentCode;
-//    }
-//
-//    private String departmentCode;
-//
-//    public LocalDate getDate() {
-//        return date;
-//    }
-//
-//    public void setDate(LocalDate date) {
-//        this.date = date;
-//    }
-//
-//    private LocalDate date;
-//
-//    public LocalTime getStartTime() {
-//        return startTime;
-//    }
-//
-//    public void setStartTime(LocalTime startTime) {
-//        this.startTime = startTime;
-//    }
-//
-//    private LocalTime startTime;
-//
-//    public LocalTime getEndTime() {
-//        return endTime;
-//    }
-//
-//    public void setEndTime(LocalTime endTime) {
-//        this.endTime = endTime;
-//    }
-//
-//    private LocalTime endTime;
-//
-//    public int getBreakDuration() {
-//        return breakDuration;
-//    }
-//
-//    public void setBreakDuration(int breakDuration) {
-//        this.breakDuration = breakDuration;
-//    }
-//
-//    private int breakDuration;
 }
